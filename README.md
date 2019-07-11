b8a1658ed75538fb5959a334d526bba13ccf4440
Jonathan Nicholas <Jonathan.Nicholas@protonmail.com>
2019-07-11 21:15:58 +1000

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TypeScript boilerplate
+
+Personal boilerplate to work with TypeScript projects on Nodejs. This boilerplate includes the necessary types, and also reinforces good commit practice with commitlint.
---
+++ index.ts
@@ -0,0 +1,12 @@
+import Express from "express";
+
+import Router from "./src/route";
+import bodyParser = require("body-parser");
+
+let app = Express();
+app.use(bodyParser.json());
+app.get("/status", (_, res): void => {
+  res.send({ date: new Date() });
+});
+app.use("/info", Router.InfoRouter);
+app.listen(3000);
